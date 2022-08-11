484135fc3fdc6c9751d04ba9814ed82858c95445
Thalles Alexsander <thallesander@hotmail.com>
2022-08-11 16:43:01 -0300

    Adding new function for the list of sounds

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-function playSoundPom () {
+function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 } 
 
-document.querySelector('.tecla_pom').onclick = playSoundPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
+let contador = 0;
 
+while (contador < 9) {
+    listaDeTeclas[contador].onclick = tocaSomPom;
+
+    contador = contador + 1;
+    
+    console.log(contador);
+}
 
